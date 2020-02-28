a483c2cf613522ee2b9212dd52905cca12dea6a7
Nick Cotter <nick.cotter@engie.com>
2020-02-28 16:49:26 +0000

    generate list of countries in select input

--- COVID19Explorer/app.R
+++ COVID19Explorer/app.R
@@ -3,18 +3,26 @@ library(dplyr)
 library(lubridate)
 library(R0)
 
-confirmed <- read.csv(url("https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"))
 
-sums <- colSums(confirmed[,-match(c("Province.State", "Country.Region", "Lat", "Long"), names(confirmed))], na.rm=TRUE)
+
+confirmed <- read.csv(url("https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"))
+confirmed <- subset(confirmed, select = -c(Lat, Long))    
+
+
+countries <- unique(confirmed$Country.Region)
+countries <- factor(append("Global", as.character(countries)))
+
+mgt <- generation.time("gamma", c(8.4, 3.8))
+
+
+sums <- colSums(confirmed[,-match(c("Province.State", "Country.Region"), names(confirmed))], na.rm=TRUE)
 sumsByDateCode <- as.data.frame(t(t(sums)))
 colnames(sumsByDateCode) <- c("count")
 sumsByDateCode$datecode <- rownames(sumsByDateCode)
 dailyCounts <- mutate(sumsByDateCode, date = mdy(substring(datecode,2)))
 dailyCounts$day <- seq.int(nrow(dailyCounts))
 
-mgt <- generation.time("gamma", c(8.4, 3.8))
 est <- estimate.R(dailyCounts$count, methods=c("TD"), GT=mgt)
-
 
 dailyCountAndPrediction <- merge(dailyCounts, est$estimates$TD$pred, by="row.names", sort=FALSE, all=TRUE)
 dailyCountAndPrediction <- dailyCountAndPrediction[-c(1)]
@@ -25,7 +33,8 @@ estimatedR <- est$estimates$TD$R[1:length(est$estimates$TD$R)-1]
 estDf <- as.data.frame(estimatedR)
 colnames(estDf) <- c("R")
 estDf$day <- rownames(estDf)
-lf <- loess(R ~ day, estDf)
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -36,11 +45,7 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-           # sliderInput("bins",
-        #                "Number of bins:",
-        #                min = 1,
-        #                max = 50,
-        #                value = 30)
+            selectInput("countries", "Country", countries)
         ),
 
         # Show a plot of the generated distribution
@@ -54,7 +59,7 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    
+
     output$dailyConfirmedPlot <- renderPlot({
         plot(dailyCountAndPrediction$day, dailyCountAndPrediction$count, xlab="days",ylab="count", col="red")
         lines(dailyCountAndPrediction$day, dailyCountAndPrediction$TD, col="green")
