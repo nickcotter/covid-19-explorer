ac8f82c6138b1f804babae3988d3104fc2c69201
Nick Cotter <nick.cotter@engie.com>
2020-02-28 14:29:25 +0000

    updated plots and removed commented out sample code

--- COVID19Explorer/app.R
+++ COVID19Explorer/app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(dplyr)
 library(lubridate)
@@ -29,20 +20,12 @@ dailyCountAndPrediction <- merge(dailyCounts, est$estimates$TD$pred, by="row.nam
 dailyCountAndPrediction <- dailyCountAndPrediction[-c(1)]
 names(dailyCountAndPrediction)[5] <- "TD"
 
-# plot(dailyCountAndPrediction$day, dailyCountAndPrediction$count, xlab="days",ylab="count", col="red")
-# lines(dailyCountAndPrediction$day, dailyCountAndPrediction$TD, col="green")
-# legend(1, max(dailyCountAndPrediction$count)-10, legend=c("Actual", "TD"), col=c("red", "green"), lty=c(0,1), pch=c(1,NA), cex=0.8)
-
 estimatedR <- est$estimates$TD$R[1:length(est$estimates$TD$R)-1]
 
 estDf <- as.data.frame(estimatedR)
 colnames(estDf) <- c("R")
 estDf$day <- rownames(estDf)
 lf <- loess(R ~ day, estDf)
-
-#plot(estDf$day, estDf$R, xlab="days", ylab="R", ylim=c(0,20), yaxt="n")
-#abline(h=1, col="gray60")
-#axis(2, at=seq(0:max(estimatedR)))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -79,19 +62,10 @@ server <- function(input, output) {
     })
     
     output$estimatedR <- renderPlot({
-        plot(estDf$day, estDf$R, xlab="days", ylab="R", ylim=c(0,20), yaxt="n")
+        plot(estDf$day, estDf$R, xlab="days", ylab="R", ylim=c(0,20), yaxt="n", pch=3)
         abline(h=1, col="gray60")
         axis(2, at=seq(0:max(estimatedR)))
     })
-
-  #  output$distPlot <- renderPlot({
-  #      # generate bins based on input$bins from ui.R
-  #      x    <- faithful[, 2]
-  #      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-#        # draw the histogram with the specified number of bins
-#        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-#    })
 }
 
 # Run the application 
