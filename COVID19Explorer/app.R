bbc1fc9c01786bf67477bf4bd64890604cc67811
Nick Cotter <nick@extropy.net>
2020-03-26 14:08:10 +0000

    added generation time input; more links and general tidying

--- COVID19Explorer/app.R
+++ COVID19Explorer/app.R
@@ -9,9 +9,9 @@ confirmed <- subset(confirmed, select = -c(Lat, Long))
 countries <- unique(confirmed$Country.Region)
 countries <- factor(append("Global", as.character(countries)))
 
-# TODO make this configurable
-mgt <- generation.time("gamma", c(5, 1.9))
-
+getGenerationTime <- function(m, s) {
+  generation.time("gamma", c(m, s))
+}
 
 getDailyCounts <- function(country) {
     
@@ -28,7 +28,7 @@ getDailyCounts <- function(country) {
         filter(count > 0)
 }
 
-generateEstimate <-function(dailyCounts) {
+generateEstimate <-function(dailyCounts, mgt) {
     estimate.R(dailyCounts$count, methods=c("TD"), GT=mgt)
 } 
 
@@ -62,26 +62,32 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
           
-            helpText("Plot case counts and estimated reproduction number globally and by country for COVID-19"),
+            helpText("Plot case counts and effective reproduction number globally and by country for COVID-19"),
           
             selectInput("countries", "Country", countries),
             
+            fluidRow(align="center", tableOutput("effectiveRSummary")),
             
-            hr(),
+            helpText("Generation Time Distribution"),
             
-            fluidRow(align="center", tableOutput("estimatedRSummary")),
+            numericInput("genTimeMean", "Mean", 5, min=0, step=0.5),
+            numericInput("genTimeStdDev", "Standard Deviation", 1.9, min=0, step=0.1),
             
-            hr(),
-          
-            p("created by",
-            a("Nick Cotter", href="https://nickcotter.com/")),
+            a("Why 5,1.9?", href="https://www.medrxiv.org/content/10.1101/2020.03.08.20032946v1.full.pdf"),
             
-            a("GitHub Repo", href="https://github.com/nickcotter/covid-19-explorer")
+            helpText("Links"),
+            
+            tags$ul(
+              tags$li("created by",a("Nick Cotter", href="https://nickcotter.com/")),
+              tags$li("made with", a("R0", href="https://bmcmedinformdecismak.biomedcentral.com/articles/10.1186/1472-6947-12-147")),
+              tags$li(a("What is generation time?", href="https://en.wikipedia.org/wiki/Generation_time")),
+              tags$li(a("GitHub Repo", href="https://github.com/nickcotter/covid-19-explorer"))
+            )
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("estimatedR"),
+            plotOutput("effectiveR"),
             plotOutput("dailyConfirmedPlot"),
             plotOutput("dailyDiffPlot")
         )
@@ -90,13 +96,17 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+    reactiveGenerationTime <- reactive({
+      getGenerationTime(input$genTimeMean, input$genTimeStdDev)
+    })
     
     reactiveDailyCounts <- reactive({
         getDailyCounts(input$countries)
     })
 
     reactiveEstimate <- reactive({
-        generateEstimate(reactiveDailyCounts())
+        generateEstimate(reactiveDailyCounts(), reactiveGenerationTime())
     })
     
     reactiveDailyCountAndPrediction <- reactive({
@@ -131,25 +141,25 @@ server <- function(input, output) {
       barplot(dailyCountAndPrediction$countDiff ~ dailyCountAndPrediction$day, xlab="days", ylab="difference", main="Confirmed Case Count Increase")
     })
     
-    output$estimatedR <- renderPlot({
+    output$effectiveR <- renderPlot({
         
         tryCatch({
             estimatedRByDay <- reactiveEstimatedRByDay()
-            plot(estimatedRByDay$day, estimatedRByDay$R, xlab="days", ylab="R", ylim=c(0,20), pch=3, main="Estimated R")
+            plot(estimatedRByDay$day, estimatedRByDay$R, xlab="days", ylab="R", ylim=c(0,20), pch=3, main="Effective R")
             abline(h=1, col="gray60")
         }, error=function(e) {})
     })
     
-    output$estimatedRSummary <- renderTable({
+    output$effectiveRSummary <- renderTable({
       
       tryCatch({
         est <- reactiveEstimate()
-        estimatedR <- est$estimates$TD$R[1:length(est$estimates$TD$R)-1]
-        as.array(summary(estimatedR))
+        effectiveR <- est$estimates$TD$R[1:length(est$estimates$TD$R)-1]
+        as.array(summary(effectiveR))
       }, error=function(e) {
       })
     
-    }, colnames = FALSE, caption="Estimated R Range", caption.placement = getOption("xtable.caption.placement", "top"))
+    }, colnames = FALSE, caption="Effective R Range", caption.placement = getOption("xtable.caption.placement", "top"))
 }
 
 # Run the application 
